b00ffbfe79b0b326d1c13a5a3aa66d8c2aed69b4
Henrique Da Silva <henriquehenry533@gmail.com>
2026-05-24 23:30:10 +0100

    transferir

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Dashboard from './pages/dashboard.tsx';
 import Extrato from "./pages/extrato.tsx";
+import Transferir from "./pages/transferir.tsx";
 
 export default function App() {
 
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/extrato" element={<Extrato />} />
+        <Route path="/transferir" element={<Transferir />} />
       </Routes>
     </BrowserRouter>   
   )
---
+++ frontend/src/pages/transferir.tsx
@@ -0,0 +1,11 @@
+import PageBase from "../components/anypageBase";
+
+
+export default function Transferir() {
+
+    return (
+        <PageBase>
+            <h1>Transferir</h1>
+        </PageBase>
+    )
+}
