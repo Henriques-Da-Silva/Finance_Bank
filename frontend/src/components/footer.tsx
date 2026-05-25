f405011c6af67b0769aa73cae519240c11f41f3c
Henrique Da Silva <henriquehenry533@gmail.com>
2026-05-25 01:09:25 +0100

    Update footer.tsx

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -5,15 +5,6 @@ export default function Footer() {
     return (
         <>
             <h1>FinanceBank</h1>
-
-export default function Pagamentos() {
-
-  return (
-    <div className="flex h-screen bg-gray-50">
-      
-    </div>
-  );
-}
         </>
     )
 }
